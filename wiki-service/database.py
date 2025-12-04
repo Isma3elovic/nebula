3bbcb7cd6264db93e6fcab59058363ec8b1db7b7
IsmaeelMohamed <ismaeelalhassan9@gmail.com>
2025-12-04 17:54:28 +0200

    fix bug

--- wiki-service/database.py
+++ wiki-service/database.py
@@ -1,6 +1,6 @@
 import os
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 # --- Connection Logic Fix ---
 # The Kubernetes Deployment YAML sets the full DATABASE_URL.
